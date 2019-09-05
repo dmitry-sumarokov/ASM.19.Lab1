eb21a13dc754e0f2a5da2cc06f386073bd1d6125
dl <dl@bugtraq.ru>
2019-09-05 15:41:23 +0300

    (!) initial commit

---
+++ asm1904/st00/main.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    from group import group
+else:
+    from .group import group
+
+
+def main():
+	group().f()
+
+
+if __name__ == '__main__':
+	main()
+
+
---
+++ lab1.py
@@ -0,0 +1,25 @@
+﻿import asm1904.st00.main
+import asm1905.st00.main
+#	добавить импорт своего модуля по шаблону 
+#	import asm<код группы>.st<номер по журналу>.main
+
+MENU = [
+		["[1904-00] Образец 1904", asm1904.st00.main.main],
+		["[1905-00] Образец 1905", asm1905.st00.main.main],
+		
+#		добавить пункт меню для вызова своей главной функции по шаблону:
+#		["[<код группы>-<номер по журналу>] <Фамилия>", <ссылка на функцию>],
+	]
+
+def menu():
+	print("------------------------------")
+	for i, item in enumerate(MENU):
+		print("{0:2}. {1}".format(i, item[0]))
+	print("------------------------------")
+	return int(input())
+
+try:
+	while True:
+		MENU[menu()][1]()
+except Exception as ex:
+	print(ex, "\nbye")
