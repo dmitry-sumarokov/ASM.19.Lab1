8db08c8e0dc1b7024213864603cacd9d29a0d74c
dmitrakov88 <31613376+dmitrakov88@users.noreply.github.com>
2019-11-01 11:58:23 +0300

    Update group.py

--- asm1904/st05/group.py
+++ asm1904/st05/group.py
@@ -7,52 +7,66 @@ from .Worker import Manager
 from .Worker import Cleaner
 
 class group:
-    def __init__(self):
-        self.group=[]
+	def __init__(self):
+		self.group=[]
 
-    def insertSeller(self):
-    	newSeller = Seller()
-    	newSeller.read()
-    	self.group.append(newSeller)
-    	print('Add new Seller')
-    	
-    def insertManager(self):
-    	newManager + Manager()
-    	newManager.read()
-    	self.group.append(newManager)
-    	print('Add new Manager')
+	def insertSeller(self):
+		newSeller = Seller()
+		newSeller.read()
+		self.group.append(newSeller)
+		print('Add new Seller')
+		
+	def insertManager(self):
+		newManager + Manager()
+		newManager.read()
+		self.group.append(newManager)
+		print('Add new Manager')
 
-    def insertCleaner(self):
-    	newCleaner = Cleaner()
-    	newCleaner.read()
-    	self.group.append(newCleaner)
-    	print('Add new Cleaner')
+	def insertCleaner(self):
+		newCleaner = Cleaner()
+		newCleaner.read()
+		self.group.append(newCleaner)
+		print('Add new Cleaner')
 
-    def execute(self):
-        self.show()
-        print('Input №: ')
-        i=int(input())
-        print('Task completed')
-        self.group[i-1].execute()
+	def execute(self):
+		self.show()
+		print('Input №: ')
+		i=int(input())
+		print('Task completed')
+		self.group[i-1].execute()
 
-    def show(self):
-        for (i,group) in enumerate (self.group):
-            print(i+1)
-            group.write()
+	def show(self):
+		for (i,group) in enumerate (self.group):
+			print(i+1)
+			group.write()
 
-    def edit(self):
-        self.show()
-        print('Input №: ')
-        i=int(input())
-        self.group[i-1].read()
-        print('Edit completed')
+	def edit(self):
+		self.show()
+		print('Input №: ')
+		i=int(input())
+		self.group[i-1].read()
+		print('Edit completed')
 
 
-    def delete(self):
-        self.show()
-        print('Input №: ')
-        i=int(input())
-        self.group.pop(i-1)
-        print('Delete completed')
+	def delete(self):
+		self.show()
+		print('Input №: ')
+		i=int(input())
+		self.group.pop(i-1)
+		print('Delete completed')
+	
+	def writefile(self):
+		f=open('asm1904/st05/group.dat','wb')
+		pickle.dump(self.group,f)
+		print('Записано в файл')
 
-    	
+	def readfile(self):
+		f=open('asm1904/st05/group.dat','rb')
+		self.group = pickle.load(f)
+		print('Считано из файла')
+	
+	def clean(self):
+		self.group.clear()
+		print('Очищено')
+
+		
